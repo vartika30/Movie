094f8ec7c5e42e377cd0bb90302f1bd39a97fb97
vartika30 <bhatt.vartika21@gmail.com>
2025-11-25 00:54:54 +0530

    second commit

---
+++ MovieByTitle.js
@@ -0,0 +1,23 @@
+import useFetch from "/useFetch"
+
+const MovieByTitle = ({title}) => {
+    const {data, loading, error} = useFetch(
+        `https://locahttp://localhost:3000/movies/${title}`)
+
+console.log(data)
+
+return data ? (
+    <div>
+        <h2>
+            {data.title}
+        </h2>
+        <p>Director:{data.director}</p>
+        <p>Country:{data.country}</p>
+        <p>Release Year:{data.releaseYear}</p>
+        <p>Rating:{data.rating}</p>
+    </div>
+):(
+loading && <p>Loading...</p>
+)
+}
+export default MovieByTitle;
